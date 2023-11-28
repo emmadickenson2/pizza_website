80c2f57cfeafa35b8c67a2e417d786b4fa99ac96
emmadickenson2 <emma.dickenson@wsu.edu>
2023-11-28 13:44:55 -0800

    added add order

--- pizzaapp/src/App.js
+++ pizzaapp/src/App.js
@@ -1,12 +1,14 @@
 import {CiPizza} from "react-icons/ci";
 import Search from "./components/Search"
+import AddOrder from "./components/AddOrder"
 
 function App() {
   return (
     <div className="App container mx-auto mt-3 font-thin">
       <h1 className="text-5xl">
         <CiPizza className="inline-block text-red-600 align-top"/>Your Orders</h1>
-      <Search/>
+        <AddOrder />
+      <Search />
     </div>
   );
 }
