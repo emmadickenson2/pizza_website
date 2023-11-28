ba8641575bdc7849b0ec7e318a06f04246216273
emmadickenson2 <emma.dickenson@wsu.edu>
2023-11-28 13:07:33 -0800

    setup tailwindcss

--- pizzaapp/src/App.js
+++ pizzaapp/src/App.js
@@ -2,8 +2,9 @@ import {CiPizza} from "react-icons/ci";
 
 function App() {
   return (
-    <div className="App">
-      <h1><CiPizza/>Your Orders</h1>
+    <div className="App container mx-auto mt-3 font-thin">
+      <h1 className="text-5xl">
+      <CiPizza className="inline-block text-red-600 align-top"/>Your Orders</h1>
     </div>
   );
 }
