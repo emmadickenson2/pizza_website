c2d2ec6e16c9d5b1c96e0b20a82d990770aff93f
emmadickenson2 <emma.dickenson@wsu.edu>
2023-11-29 10:15:48 -0800

    put order info in component

--- pizzaapp/src/App.js
+++ pizzaapp/src/App.js
@@ -1,7 +1,7 @@
 import {CiPizza} from "react-icons/ci";
-import {BiTrash} from "react-icons/bi";
 import Search from "./components/Search"
 import AddOrder from "./components/AddOrder"
+import OrderInfo from "./components/OrderInfo";
 import orderList from "./data.json"
 
 function App() {
@@ -15,19 +15,8 @@ function App() {
       <ul className="divie-y divide-gray-200">
         {orderList
           .map(order => (
-            <li className="px-3 py-3 flex items-start">
-              <button type="button"
-                className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
-                <BiTrash /></button>
-              <div className="flex-grow">
-                <div className="flex items-center">
-                  <span className="flex-none font-medium text-2xl text-blue-500">{order.name}</span>
-                  <span className="flex-grow text-right">{order.orderDate}</span>
-                </div>
-                <div><b className="font-bold text-blue-500">Order:</b> {order.order}</div>
-                <div className="leading-tight">{order.orderNotes}</div>
-              </div>
-            </li>
+            <OrderInfo key={order.id}
+            order={order}/>
           ))
         }
       </ul>
---
+++ pizzaapp/src/components/OrderInfo.js
@@ -0,0 +1,20 @@
+import {BiTrash} from "react-icons/bi";
+const OrderInfo = ({order}) => {
+    return (
+        <li className="px-3 py-3 flex items-start">
+            <button type="button"
+            className="p-1.5 mr-1.5 mt-1 rounded text-white bg-red-500 hover:bg-yellow-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-500">
+            <BiTrash /></button>
+            <div className="flex-grow">
+            <div className="flex items-center">
+                <span className="flex-none font-medium text-2xl text-blue-500">{order.name}</span>
+                <span className="flex-grow text-right">{order.orderDate}</span>
+            </div>
+            <div><b className="font-bold text-blue-500">Order:</b> {order.order}</div>
+            <div className="leading-tight">{order.orderNotes}</div>
+            </div>
+        </li>
+    )
+}
+
+export default OrderInfo
